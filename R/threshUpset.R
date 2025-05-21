30c91aac30fbd694066330b49af2496c7ce9e18f
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-05-21 15:21:07 -0500

    linting

--- R/threshUpset.R
+++ R/threshUpset.R
@@ -1,14 +1,7 @@
 #' Function to plot results of a changepoint models as upset plots
 #'
 #' @param thresh Output from \code{\link{thresh}}
-#' @param mode Either "phenotype" (the default) or "pvalue". "phenotype" will show an upset plot
-#' of significant changepoint models against the phenotypes present in thresh, "pvalue" will show
-#' the results of various p-value adjustment methods.
-#' @param pvalcol The column in thresh that has p-value data, defaults to "p.value." which reflects
-#' standard output from \code{thresh}.
 #' @param cutoff Alpha, the significance threshold. Defaults to 0.05.
-#' @param cores Number of cores to use in parallel. This should not make much of a difference generally
-#' but can be useful with very large tables. Defaults to 1 if the "mc.cores" option is not set.
 #'
 #' @keywords changepoint, threshold, regression, phenotype, upset
 #'
@@ -29,12 +22,10 @@
 #' @export
 
 threshUpset <- function(thresh, cutoff = 0.05) {
-  
   sig <- unlist(lapply(unique(thresh$predictor), function(pred) {
     sub <- thresh[thresh$predictor == pred]
     sig_index <- which(unlist(lapply(sub$slope, function(x) x$padj)) < cutoff)
     if (length(sig_index) > 0) {
-      #d <- data.frame(predictor = asv, pheno = sub$phenotype[sig_index])
       return(pred)
     }
   }))
@@ -48,8 +39,8 @@ threshUpset <- function(thresh, cutoff = 0.05) {
   d$sig <- ifelse(d$sig < cutoff, TRUE, FALSE)
   data.table::setDT(d)
   upsetPlotData <- as.data.frame(data.table::dcast(d,
-                                                   predictor ~ phenotype,
-                                                   value.var = "sig"
+    predictor ~ phenotype,
+    value.var = "sig"
   ))
   p <- ComplexUpset::upset(upsetPlotData, intersect = colnames(upsetPlotData)[-1])
   p[[2]] <- p[[2]] +
@@ -64,4 +55,3 @@ threshUpset <- function(thresh, cutoff = 0.05) {
   return(p)
   return(p)
 }
-
